41e657aa97d72cae042be1d9532f93f1c02ea596
Jamie Thompson <bishbashboshjt@gmail.com>
2020-02-24 17:32:25 +0100

    fast forward tasty-reader to dotty 0.21.0-RC1 compatible

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   lazy val junitInterface = "com.novocode"    % "junit-interface"     % "0.11"
-  lazy val zio            = "dev.zio"         % "zio_0.20"            % "1.0.0-RC17+184-5db6514b"//"1.0.0-RC17"
-  lazy val dottyLib       = "ch.epfl.lamp"    % "dotty-library_0.20"  % "0.20.0-RC1"
-  lazy val intent         = "com.factor10"    % "intent_0.20"         % "0.3.0"
-  lazy val scalacheck     = "org.scalacheck"  % "scalacheck_0.20"     % "1.14.1-SNAPSHOT"
+  lazy val zio            = "dev.zio"         % "zio_0.21"            % "1.0.0-RC17+184-5db6514b+20200224-1650"//"1.0.0-RC17"
+  lazy val dottyLib       = "ch.epfl.lamp"    % "dotty-library_0.21"  % "0.21.0-RC1"
+  lazy val intent         = "com.factor10"    % "intent_0.21"         % "0.4.0"
+  lazy val scalacheck     = "org.scalacheck"  % "scalacheck_0.21"     % "1.14.1-SNAPSHOT"
 }
