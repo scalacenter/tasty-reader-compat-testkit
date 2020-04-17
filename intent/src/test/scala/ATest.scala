ede5346cddfe6e30b75027e1e7c1c22013b9ee37
Jamie Thompson <bishbashboshjt@gmail.com>
2020-04-18 00:42:26 +0200

    fast forward tasty-reader to dotty 0.23.0-RC1 compatible

--- intent/src/test/scala/ATest.scala
+++ intent/src/test/scala/ATest.scala
@@ -1,8 +1,13 @@
 import intent._
 
-
-class ATest extends TestSuite with Stateless {
-  apply("3 + 3") {
-    in("should be 6")(toEqual(expect(3 + 3))(6))
+class ATest extends TestSuite with Stateless { self =>
+  import self.{stringExt => test}, test._
+  implicit val pos: intent.macros.Position        = ??? // comment line for illegal macro
+  implicit val Eq: intent.core.Eq[Int]            = ??? // comment line for ambiguous implicit
+  implicit val format: intent.core.Formatter[Int] = ??? // comment line for ambiguous implicit
+  test("3 + 3") {                                       // illegal union type intent.core.MapLike
+    in("should be 6") {
+      toEqual(expect(3 + 3))(6)
+    }
   }
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val junitInterface = "com.novocode"   % "junit-interface" % "0.11"
-  lazy val zio            = "dev.zio"        % "zio_0.22"        % "1.0.0-RC17-tastycompat"
-  lazy val intent         = "com.factor10"   % "intent_0.22"     % "0.5.0"
-  lazy val scalacheck     = "org.scalacheck" % "scalacheck_0.22" % "1.14.1-SNAPSHOT"
+  lazy val zio            = "dev.zio"        % "zio_0.23"        % "1.0.0-RC17-tastycompat"
+  lazy val intent         = "com.factor10"   % "intent_0.23"     % "0.6.0"
+  lazy val scalacheck     = "org.scalacheck" % "scalacheck_0.23" % "1.14.1-SNAPSHOT"
 }
