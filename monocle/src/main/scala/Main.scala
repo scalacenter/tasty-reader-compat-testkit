19dca15c1be984cc06a0f7053fb5a0b0d28182ba
Jamie Thompson <bishbashboshjt@gmail.com>
2021-03-02 16:40:25 +0100

    rewrite extension methods

--- monocle/src/main/scala/Main.scala
+++ monocle/src/main/scala/Main.scala
@@ -1,9 +1,10 @@
 
-import monocle.syntax.all._
+// import monocle.syntax.all._
+import monocle._
 
 
 object Main extends App{
-  
+
 
     case class Address(number: Int, street: String)
 
@@ -11,12 +12,15 @@ object Main extends App{
 
     val user = User("Anna", Address(12, "high street"))
 
-    user.focus(_.name).replace("Bob")
+    // user.focus(_.name).replace("Bob")
+    Focus.focus(user)(_.name).replace("Bob")
     // res: User = User("Bob", Address(12, "high street"))
 
-    user.focus(_.address.streetName).modify(_.toUpperCase)
+    // user.focus(_.address.streetName).modify(_.toUpperCase)
+    Focus.focus(user)(_.address.streetName).modify(_.toUpperCase)
     // res: User = User("Anna", Address(12, "HIGH STREET"))
 
-    user.focus(_.address.streetNumber).get
+    // user.focus(_.address.streetNumber).get
+    Focus.focus(user)(_.address.streetNumber).get
     // res: Int = 12
 }
